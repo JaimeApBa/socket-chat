1fe91064de28b106091245897d4fd11450319e43
Jaime Aparicio <Jaime@OBRELSA.local>
2018-08-07 17:40:09 +0200

    chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -3,23 +3,25 @@ var socket = io();
 var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
-    window.location = "index.html";
-    throw new Error('El nombre y la sala son necesario');
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
 
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
-
+    socket.emit('entrarChat', usuario, function(personas) {
+        // console.log('Usuarios conectados', resp);
+        renderizarUsuarios(personas);
     });
+
 });
 
 // escuchar
@@ -32,7 +34,7 @@ socket.on('disconnect', function() {
 
 // Enviar información
 // socket.emit('crearMensaje', {
-//     usuario: 'Fernando',
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -40,21 +42,20 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-// escuchar entradas/salidas de los usuarios del chat
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
     console.log(personas);
-
 });
 
 // Mensajes privados
-
 socket.on('mensajePrivado', function(mensaje) {
 
-    console.log('Mensaje Privado: ', mensaje);
+    console.log('Mensaje Privado:', mensaje);
+
 });
